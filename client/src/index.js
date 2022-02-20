6d9dd76ecc57e49929194e1940db84726ef69949
darke <kalvinharriscs@gmail.com>
2022-02-20 13:01:09 -0500

    QR scanner

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App text= "https://www.tacklhealth.com/tackl-corporate-testing" />
   </React.StrictMode>,
   document.getElementById('root')
 );
